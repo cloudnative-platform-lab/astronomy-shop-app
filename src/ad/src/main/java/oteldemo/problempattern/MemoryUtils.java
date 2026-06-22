d40f258049be736b33ec58b82795ffdb9c8e53dc
Prasanna Meenamanalan <prasanna.m.loa@gmail.com>
2026-06-22 11:11:53 +0530

    Refactor MemoryUtils for improved readability

--- src/ad/src/main/java/oteldemo/problempattern/MemoryUtils.java
+++ src/ad/src/main/java/oteldemo/problempattern/MemoryUtils.java
@@ -1,8 +1,3 @@
-/*
-* Copyright The OpenTelemetry Authors
-* SPDX-License-Identifier: Apache-2.0
-*/
-
 package oteldemo.problempattern;
 
 import java.lang.management.MemoryMXBean;
@@ -10,56 +5,54 @@ import java.lang.management.MemoryUsage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 /**
  * This class provides JVM heap related utility methods.
-*/
+ */
 public class MemoryUtils {
 
-    private static final Logger logger = LogManager.getLogger(MemoryUtils.class.getName());
+  private static final Logger logger = LogManager.getLogger(MemoryUtils.class.getName());
 
-    private static final long NO_HEAP_LIMIT = -1;
+  private static final long NO_HEAP_LIMIT = -1;
 
-    private final MemoryMXBean memoryBean;
+  private final MemoryMXBean memoryBean;
 
-    /**
-     * @param memoryBean defines which {@link MemoryMXBean} is to use
-    */
-    public MemoryUtils(MemoryMXBean memoryBean) {
-        this.memoryBean = memoryBean;
+  /**
+   * @param memoryBean defines which {@link MemoryMXBean} is to use
+   */
+  public MemoryUtils(MemoryMXBean memoryBean) {
+    this.memoryBean = memoryBean;
+  }
+
+  /**
+   * @return The current heap usage as a decimal number between 0.0 and 1.0. That is, if the
+   *     returned value is 0.85, 85% of the max heap is used.
+   *     <p>If no max heap is set, the method returns -1.0.
+   */
+  public double getHeapUsage() {
+    MemoryUsage heapProps = memoryBean.getHeapMemoryUsage();
+    long heapUsed = heapProps.getUsed();
+    long heapMax = heapProps.getMax();
+
+    if (heapMax == NO_HEAP_LIMIT) {
+      if (logger.isDebugEnabled()) {
+        logger.debug("No maximum heap is set");
+      }
+      return NO_HEAP_LIMIT;
     }
 
+    double heapUsage = (double) heapUsed / heapMax;
 
-    /**
-     * @return The current heap usage as a decimal number between 0.0 and 1.0.
-    *         That is, if the returned value is 0.85, 85% of the max heap is used.
-    *
-    *         If no max heap is set, the method returns -1.0.
-    */
-    public double getHeapUsage() {
-        MemoryUsage heapProps = memoryBean.getHeapMemoryUsage();
-        long heapUsed = heapProps.getUsed();
-        long heapMax = heapProps.getMax();
-
-        if (heapMax == NO_HEAP_LIMIT) {
-            if (logger.isDebugEnabled()) {
-            logger.debug("No maximum heap is set");
-            }
-            return NO_HEAP_LIMIT;
-        }
-
-
-        double heapUsage = (double) heapUsed / heapMax;
-        if (logger.isDebugEnabled()) {
-            logger.debug("Current heap usage is {0} percent" + (heapUsage * 100));
-        }
-        return heapUsage;
+    if (logger.isDebugEnabled()) {
+      logger.debug("Current heap usage is {} percent", heapUsage * 100);
     }
 
-    /**
-     * see {@link MemoryMXBean#getObjectPendingFinalizationCount()}
-    */
-    public int getObjectPendingFinalizationCount() {
-        return memoryBean.getObjectPendingFinalizationCount();
-    }
+    return heapUsage;
+  }
+
+  /**
+   * see {@link MemoryMXBean#getObjectPendingFinalizationCount()}
+   */
+  public int getObjectPendingFinalizationCount() {
+    return memoryBean.getObjectPendingFinalizationCount();
+  }
 }
